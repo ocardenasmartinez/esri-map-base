62176d9bec577f9d8cfb4e58c2cf7903b5bf0ac3
Octavio Ernesto Cárdenas Martínez <ocardenas@subtel.gob.cl>
2018-11-08 16:13:46 -0300

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EsriMapComponent } from './esri-map/esri-map.component';
 
@@ -10,7 +11,8 @@ import { EsriMapComponent } from './esri-map/esri-map.component';
     EsriMapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
